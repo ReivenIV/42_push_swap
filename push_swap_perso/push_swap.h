2dee8e8af441f8faff29b2bf8baa5d1a7079d83b
ReivenIV <runyalen88@duck.com>
2025-01-27 15:34:05 +0200

    .h updates

--- push_swap_perso/push_swap.h
+++ push_swap_perso/push_swap.h
@@ -1,8 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rita <rita@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 14:28:11 by rita              #+#    #+#             */
+/*   Updated: 2025/01/27 14:28:11 by rita             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include <stdio.h>
+# include <stdio.h>   //! <--
 # include <stdlib.h>
+# include <stdbool.h> //* <--
+# include <unistd.h>
+# include <limits.h>
+
+typedef struct s_stack
+{
+	int				value;
+	int				current_position;
+//	int				final_index;
+//	int				push_price;
+//	bool			above_median;
+//	bool			cheapest;
+//	struct s_stack	*target_node;
+	struct s_stack	*previous;
+	struct s_stack	*next;
+}	t_stack;
 
 // Tools  
 //* Parsing
@@ -10,6 +38,8 @@ char	**ft_split(char const *src);
 long	ft_atol(const char *src);
 
 // Error handling
+int		syntax_check(char* src);
+void	free_argv(char **argv);
 
 // Algos
 
