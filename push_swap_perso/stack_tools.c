5fa56def031ea852e12b971c1ac473100b5d5af3
ReivenIV <runyalen88@duck.com>
2025-01-28 16:32:10 +0200

    stack_tools updated, code more readable. get_last_node, push_to_bottom_node, push_to_top_node and main to test created

--- push_swap_perso/stack_tools.c
+++ push_swap_perso/stack_tools.c
@@ -12,39 +12,129 @@
 
 #include "push_swap.h"
 
+/*
+	- *stack			 :: is a pointer to the top node of the stack.
+	- (*stack)			 :: dereferences this pointer to access the actual node.
+	- (*stack)->previous :: accesses the previous pointer of this node.
+*/
+
 //* Function to print all elements in the stack
-void print_stack(t_stack *stack)
+void print_stack(t_stack *stack)  			//! FOR TESTING ONLY 
 {
-    t_stack *current = stack;
+    t_stack *current;
+
+	current = stack;
     while (current != NULL)
     {
-        printf("%d ", current->value);
+        printf("%d ", current->value);		//! PRINTF
         current = current->next;
     }
-    printf("\n");
+    printf("\n");							//! PRINTF
 }
 
 
 //* creates new node
-t_stack *new_node(int value)
+t_stack *create_new_node(int value)
 {
-    t_stack *node = malloc(sizeof(t_stack));
-    if (!node)
+    t_stack *node;
+
+	node = malloc(sizeof(t_stack));
+    if (node == NULL)
         return NULL;
     node->value = value;
     node->next = NULL;
-    node->previous = NULL;
+    node->prev = NULL;
     return node;
 }
+//* Find last node in a stack. 
+// We don't need the 2 variables created i just add them for comprehention.
+t_stack	*get_last_node(t_stack	*head_node)
+{
+	t_stack	*current_node;
+	t_stack	*last_node;
+
+	current_node = head_node;
+	if (current_node == NULL)
+		return (NULL);
+	while (current_node->next)
+		current_node = current_node->next;
+	last_node = current_node;
+	return (last_node);
+}
+
+// TODO Append one node at the end of the stack
+// Will add a node to the bottom of the stack. 
+// ex: Stack : (1,2,3)   to add : 40  ==>  Stack : (1,2,3,40)
+void	push_to_bottom_node(t_stack **stack, int value)     
+{
+	t_stack	*new_node;
+	t_stack	*old_last_node;
+
+	if (stack == NULL)
+		return ;
+	new_node = create_new_node(value);						// We don't need to update new_node->next/prev create_new_node set them to NULL 
+	if (new_node == NULL)
+		return ;
+	if (*stack == NULL)
+		*stack = new_node;									// If the stack is null we add our new node on top.
+	else
+	{
+		old_last_node = get_last_node(*stack);				
+		old_last_node->next = new_node;
+		new_node->prev = old_last_node;
+	}
+}
+
+
 
 //* Function to add a node to the stack
-void push_node(t_stack **stack, int value)
+// Will add a node on top of the stack. 
+// ex: Stack : (1,2,3)   to add : 40  ==>  Stack : (40,1,2,3)
+void push_to_top_node(t_stack **stack, int value)
 {
-    t_stack *node = new_node(value);
-    if (!node)
-        return;
-    node->next = *stack;
-    if (*stack)
-        (*stack)->previous = node;
-    *stack = node;
+	t_stack *new_node;
+
+	new_node = create_new_node(value);
+	if (new_node == NULL)
+		return ;
+	t_stack *top_node = *stack; 			// get the current head node of the stack.
+	new_node->next = top_node;				// set the new node next adrrese to the current top node.
+	if (top_node)
+		top_node->prev = new_node;			// if the stack is not empty the new secod node now will point to the new node
+	*stack = new_node;						// updates the stack top pointer with the current new_node created.
 }
+
+
+
+//* test | get_last_node | push_to_bottom_node | push_to_top_node | to tests.
+// // int main()
+// // {
+// //     t_stack *stack = NULL;
+// //     // Add some nodes to the stack using push_to_top_node or push_to_bottom_node to test. 
+// //     push_to_bottom_node(&stack, 1);
+// //     push_to_bottom_node(&stack, 2);
+// //     push_to_bottom_node(&stack, 3);
+// //     push_to_bottom_node(&stack, 4);
+// //     push_to_bottom_node(&stack, 500);
+// //     // Print the stack
+// //     printf("Stack elements: ");
+// //     print_stack(stack);
+// //     // Get the last node
+// //     t_stack *last_node = get_last_node(stack);
+// //     // Print the value of the last node
+// //     if (last_node)
+// //         printf("The last node value is: %d\n", last_node->value);
+// //     else
+// //         printf("The stack is empty.\n");
+// //     // Free the stack (for good practice)
+// //     t_stack *current = stack;
+// //     t_stack *next;
+// //     while (current)
+// //     {
+// //         next = current->next;
+// //         free(current);
+// //         current = next;
+// //     }
+// //     return 0;
+// // }
+
