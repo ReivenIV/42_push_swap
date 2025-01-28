4ffd94dcc91373f0ea245c6165200a7a3a0b7397
ReivenIV <runyalen88@duck.com>
2025-01-28 18:25:25 +0200

    .h sintax errors

--- push_swap_perso/push_swap.h
+++ push_swap_perso/push_swap.h
@@ -36,20 +36,22 @@ typedef struct s_stack
 //* Parsing
 char		**ft_split(char const *src);
 long		ft_atol(const char *src);
+void		stack_init(t_stack **stack_a, char **argv, bool is_argc_2);
+
 
 // Error handling
-int			syntax_check(char* src);
-int			is_nb_in_stack(t_stack *a, int nb);
+int			is_syntax_correct(char* src);									// return BOOl.
+int			is_nb_in_stack(t_stack *a, int nb);								// return BOOl.
 void		free_stack(t_stack **stack);
 void		free_argv(char **argv);
 void		free_handler(t_stack **a_or_b, char **argv, bool flag_argc_2);
 
 // Stack/node tools
-void		print_stack(t_stack *stack);			// --> for testing
+void		print_stack(t_stack *stack);					// --> for testing
 t_stack		*create_new_node(int value);					// --> for testing
 t_stack		*get_last_node(t_stack	*head_node);
 void		push_to_top_node(t_stack **stack, int value);
-void	push_to_bottom_node(t_stack **stack, int value);
+void		push_to_bottom_node(t_stack **stack, int value);
 
 
 // Algos
