756f9cce72103391003ea4d1e6433e9d370ff912
ReivenIV <runyalen88@duck.com>
2025-02-06 14:25:59 +0200

    main commented for testing

--- push_swap_perso/main.c
+++ push_swap_perso/main.c
@@ -22,30 +22,30 @@ void print_argv(char **av)
     }
 }
 
-int	main(int ac, char **av)
-{
-	t_stack	*a;
-	t_stack	*b;
+// int	main(int ac, char **av)
+// {
+// 	t_stack	*a;
+// 	t_stack	*b;
 
-	a = NULL;
-	b = NULL;
-	if (ac == 1 || (ac == 2 && !av[1][0]))				// if ac doesn't have arguments or If ac has arguments but av[1] is empty
-		return (1);
-	else if (ac == 2)
-	{
-		av = ft_split(av[1]);
-		stack_init(&a, av, true);
-	}
-	else
-		stack_init(&a, av + 1, false); 					// av + 1 ?
-	if (!is_stack_sorted(a))
-	{
-		if (get_stack_len(a) == 2)
-			sa(&a, false);
-		else if (get_stack_len(a) == 3)
-			sort_three_nbs(&a);
-		//else if (get_stack_len(a) > 3)
-			//push_swap algo and we handle 4 & 5 and infinit inside 
-	}
-	free_stack(&a);
-}
+// 	a = NULL;
+// 	b = NULL;
+// 	if (ac == 1 || (ac == 2 && !av[1][0]))				// if ac doesn't have arguments or If ac has arguments but av[1] is empty
+// 		return (1);
+// 	else if (ac == 2)
+// 	{
+// 		av = ft_split(av[1]);
+// 		stack_init(&a, av, true);
+// 	}
+// 	else
+// 		stack_init(&a, av + 1, false); 					// av + 1 ?
+// 	if (!is_stack_sorted(a))
+// 	{
+// 		if (get_stack_len(a) == 2)
+// 			sa(&a, false);
+// 		else if (get_stack_len(a) == 3)
+// 			sort_three_nbs(&a);
+// 		//else if (get_stack_len(a) > 3)
+// 			//push_swap algo and we handle 4 & 5 and infinit inside 
+// 	}
+// 	free_stack(&a);
+// }
