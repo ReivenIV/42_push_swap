34bb6ac1771156ba41cbeb851aa2b53e55fcf3df
ReivenIV <runyalen88@duck.com>
2025-02-07 16:47:50 +0200

    sort_5 fixbug

--- push_swap_perso/algo_5_nbs.c
+++ push_swap_perso/algo_5_nbs.c
@@ -22,30 +22,35 @@ void	sort_five_nbs(t_stack **stack_a, t_stack **stack_b)
 	refresh_ids(*stack_a);
 	smallest_node = get_smallest(*stack_a);
 	move_t_to_top(stack_a, smallest_node, 'a');
+	pb(stack_a, stack_b, false);
+
 	sort_four_nbs(stack_a, stack_b);
-	pb(stack_b, stack_a, false);
-	pb(stack_b, stack_a, false);
+	pa(stack_b, stack_a, false);
+	pa(stack_b, stack_a, false);
 }
 
-// // int main(void)
-// // {
-// //     t_stack *stack_a = NULL;
-// //     t_stack *stack_b = NULL;
-// //     // Add four random numbers to stack_a
-// //     push_to_bottom_node(&stack_a, 300);
-// //     push_to_bottom_node(&stack_a, 1);
-// //     push_to_bottom_node(&stack_a, 4);
-// //     push_to_bottom_node(&stack_a, 2);
-// //     push_to_bottom_node(&stack_a, 50);
-// //     // Print the stack before sorting
-// // 	//print_stack_json(stack_a, 'a');
-// // 	//printf("------------------------");
-// //     // Sort the stack
-// //     sort_five_nbs(&stack_a, &stack_b);
-// //     // Print the stack after sorting
-// // 	//print_stack_json(stack_a, 'a');
-// //     // Free the stacks
-// //     free_stack(&stack_a);
-// //     free_stack(&stack_b);
-// //     return 0;
-// // }
+//*   ---------------------
+//*   ::  test it dear   ::
+//*  ----------------------
+// int main(void)
+// {
+//     t_stack *stack_a = NULL;
+//     t_stack *stack_b = NULL;
+//     // Add four random numbers to stack_a
+//     push_to_bottom_node(&stack_a, 300);
+//     push_to_bottom_node(&stack_a, 1);
+//     push_to_bottom_node(&stack_a, 4);
+//     push_to_bottom_node(&stack_a, 2);
+//     push_to_bottom_node(&stack_a, 50);
+//     // Print the stack before sorting
+// 	//print_stack_json(stack_a, 'a');
+// 	//printf("------------------------");
+//     // Sort the stack
+//     sort_five_nbs(&stack_a, &stack_b);
+//     // Print the stack after sorting
+// 	//print_stack_json(stack_a, 'a');
+//     // Free the stacks
+//     free_stack(&stack_a);
+//     free_stack(&stack_b);
+//     return 0;
+// }
