fb1b03e993d83072a4bcabb663a029c3fb468b12
ReivenIV <runyalen88@duck.com>
2025-02-10 10:50:20 +0200

    move_cheapest fixbug

--- push_swap_perso/tools_move_stack.c
+++ push_swap_perso/tools_move_stack.c
@@ -33,18 +33,18 @@ void	move_t_to_top(t_stack **stack, t_stack *target_node, char s_group)
 		}
 	}
 }
-
+// Will bring cheapest in stack_B to the top
 void	move_cheapest(t_stack **a, t_stack **b)
 {
 	t_stack	*cheapest;
 
-	cheapest = get_cheapest(*b);
+	cheapest = get_cheapest(*b);														// Find cheapest node in stack_b
 
-	if (cheapest->is_above_median && cheapest->target_node->is_above_median)
+	if (cheapest->is_above_median && cheapest->target_node->is_above_median)			// to reduces amount of moves we rr or rrr both stacks to bring the cheapest to the top
 		rr_till_cheapest(a, b, cheapest);
 	else if (!(cheapest->is_above_median) && !(cheapest->target_node->is_above_median))
 		rrr_till_cheapest(a, b, cheapest);
-	move_t_to_top(b, cheapest, 'b');
-	move_t_to_top(a, cheapest->target_node, 'a');
-	pa(a, b, false);
+	move_t_to_top(b, cheapest, 'b');													// Then we move individually node in stack_B to the top
+	move_t_to_top(a, cheapest->target_node, 'a');										// same we move individually node in stack_A to the top
+	pa(b, a, false);																	// at the end when they are alineated we push to stack_A
 }
