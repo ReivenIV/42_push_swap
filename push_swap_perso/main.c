f0a3703797113eb4f60f6d44d18ea3f5c2dbd947
ReivenIV <runyalen88@duck.com>
2025-02-10 13:25:31 +0200

    main updated, tested and working

--- push_swap_perso/main.c
+++ push_swap_perso/main.c
@@ -12,27 +12,24 @@
 
 #include "push_swap.h"
 
-// int	main(int ac, char **av)
-// {
-// 	t_stack	*stack_a;
-// 	t_stack	*stack_b;
+int	main(int ac, char **av)
+{
+	t_stack	*stack_a;
+	t_stack	*stack_b;
 
-// 	stack_a = NULL;
-// 	stack_b = NULL;
-// 	if (ac == 1 || (ac == 2 && !av[1][0]))				// if ac doesn't have arguments or If ac has arguments but av[1] is empty
-// 		return (1);
-// 	else if (ac == 2)
-// 	{
-// 		av = ft_split(av[1]);
-// 		stack_init(&stack_a, av, true);
-// 	}
-// 	else
-// 		stack_init(&stack_a, av + 1, false); 					// av + 1 ?
-// 	if (!is_stack_sorted(stack_a))
-// 		algo_handler(&stack_a, &stack_b);
-
-// 	printf("----------------------");	
-// 	print_stack_json(stack_a, 'a');
-// 	print_stack_json(stack_b, 'b');
-// 	free_stack(&stack_a);
-// }
+	stack_a = NULL;
+	stack_b = NULL;
+	if (ac == 1 || (ac == 2 && !av[1][0]))				// if ac doesn't have arguments or If ac has arguments but av[1] is empty
+		return (1);
+	else if (ac == 2)
+	{
+		av = ft_split(av[1]);
+		stack_init(&stack_a, av, true);
+	}
+	else
+		stack_init(&stack_a, av + 1, false); 					// av + 1 ?
+	if (!is_stack_sorted(stack_a))
+		algo_handler(&stack_a, &stack_b);
+	free_stack(&stack_a);
+	free_stack(&stack_b);
+}
